1a33dc82327502d46a275ddd97b5c7e90bed8363
srtarun <srtarun17@gmail.com>
2017-04-05 11:17:44 +0530

    server

--- couch/config.php
+++ couch/config.php
@@ -48,7 +48,7 @@
     define( 'K_DB_PASSWORD', 'foodblog' );
     // 7.
     // MySQL hostname (it will usually be 'localhost')
-    define( 'K_DB_HOST', 'ftp.gear.host' );
+    define( 'K_DB_HOST', 'localhost' );
     // 7b.
     // Needed only if multiple instances of this CMS are to be installed in the same database
     // (please use only alphanumeric characters or underscore (NO hyphen))
